39414765b6ebb5bc7d952f2a0729b772f6710afe
DiegoSG22 <dpardo47@gmail.com>
2025-11-23 19:35:42 -0300

    Auto-crear superusuario en build

--- build.sh
+++ build.sh
@@ -9,3 +9,5 @@ python manage.py collectstatic --no-input
 
 # Aplicar migraciones a la base de datos de la nube
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'Monfer2025') if not User.objects.filter(username='admin').exists() else print('El Superusuario ya existe')"
